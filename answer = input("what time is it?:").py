332d612bbf4567bcaba01fb6489e769feb5013cb
Kristjan .L <92045997+Kris-tjan@users.noreply.github.com>
2022-02-14 17:27:33 +0200

    Add files via upload

---
+++ answer = input("what time is it?:").py
@@ -0,0 +1,12 @@
+linned = ["kuldnok","vared","päsuke","kana"]
+print("kuldnok")
+print ("linned")
+linnud = ("jaanulund,vares,päsuk")
+if("tuvi") in list:
+    print("vuvi on list")
+else:
+    print("luviei ole")
+
+linned = ["kuldnok","vares,päsuke,kana"]
+for x in linned:
+    print(x)
